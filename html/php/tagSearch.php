8ab85ec39e93b262098e5e00f553239c54fbf2f6
Benjamin Loong Thomas <benjamin@thomasnetwork.net>
2015-07-21 00:27:35 -0400

    added tag Search

---
+++ html/php/tagSearch.php
@@ -0,0 +1,38 @@
+<?php
+  require dirname(__FILE__).'/mysql-connect.php';
+  openConnection();
+
+  if(!isset($_GET['q'])) {
+    echo '[]';
+    return;
+  }
+  $q = mysqli_escape_string($connection, $_GET['q']);
+  $query = 'SELECT t.id, t.name, e.title as episodeTitle, e.id as episodeID FROM (tag t LEFT JOIN tagLink tl on t.id = tl.tagID) LEFT JOIN episode e on e.id = tl.episodeID WHERE t.name LIKE \'%'.$q.'%\'; ';
+  $result = runQuery($query);
+
+  closeConnection();
+  $tags = array();
+
+  while($row = mysqli_fetch_assoc($result)) {
+    if(isset($row['episodeTitle']) && $row['episodeID']) {
+      $episode = array(
+        'id' => intval($row['episodeID']),
+        'title' => $row['episodeTitle']
+      );
+    }
+    unset($row['episodeTitle']);
+    unset($row['episodeID']);
+    $row['id'] = intval($row['id']);
+    if(!isset($tags[$row['id']])) {
+      $row['episodes'] = array();
+      $tags[$row['id']] = $row;
+    }
+
+    if(isset($episode)) {
+      $tags[$row['id']]['episodes'][] = $episode;
+    }
+    unset($episode);
+  }
+
+  echo json_encode(array_values($tags));
+ ?>
